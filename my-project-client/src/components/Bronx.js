262b16a1cf3c224f0c950c019a844b724a6a8aa2
github username <franchely811@gmail.com>
2019-08-19 15:07:11 -0400

    starts routing

---
+++ my-project-client/src/components/Bronx.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+
+export default class Bronx extends Component {
+
+    render() {
+        return (
+            <div>
+                <h2>
+                    Welcome to The Bronx!
+                </h2>
+            </div>
+        )
+    }
+}
