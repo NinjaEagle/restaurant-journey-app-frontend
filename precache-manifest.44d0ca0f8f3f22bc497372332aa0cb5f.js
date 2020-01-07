a54717dbde62639e70b954e509a6b11b14836dbd
Kevin Wang <kcwang06@gmail.com>
2020-01-06 21:10:03 -0500

    Updates

---
+++ precache-manifest.44d0ca0f8f3f22bc497372332aa0cb5f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "570d7fc89f53a4dd8f7c8d45d6f27413",
+    "url": "/restaurant-journey-app/index.html"
+  },
+  {
+    "revision": "cc1f2c7ca88540643341",
+    "url": "/restaurant-journey-app/static/css/main.68abee3f.chunk.css"
+  },
+  {
+    "revision": "31a6a0b17f3beebf0151",
+    "url": "/restaurant-journey-app/static/js/2.bb72eaf8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/restaurant-journey-app/static/js/2.bb72eaf8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc1f2c7ca88540643341",
+    "url": "/restaurant-journey-app/static/js/main.f304eecc.chunk.js"
+  },
+  {
+    "revision": "c29e4144cbe73b13d08d",
+    "url": "/restaurant-journey-app/static/js/runtime-main.511b13fe.js"
+  },
+  {
+    "revision": "fcf27836aa2c3eb95a9f7037d49232b6",
+    "url": "/restaurant-journey-app/static/media/brooklyn.fcf27836.jpg"
+  },
+  {
+    "revision": "b286e9f7e209b80a4a96fb7cc3555fef",
+    "url": "/restaurant-journey-app/static/media/fordham-road.b286e9f7.jpg"
+  },
+  {
+    "revision": "eb976ba66af5de29cf175e6ba321d3ea",
+    "url": "/restaurant-journey-app/static/media/manhattan.eb976ba6.jpg"
+  },
+  {
+    "revision": "441423d1c2b0321acacb9e3b110be6af",
+    "url": "/restaurant-journey-app/static/media/queens.441423d1.jpg"
+  },
+  {
+    "revision": "067766d4e022192902271eae2f691ed1",
+    "url": "/restaurant-journey-app/static/media/statenisland.067766d4.jpeg"
+  }
+]);
