ba35d276047ac4841cccd1936171988eecf5a06a
Kevin Wang <kcwang06@gmail.com>
2020-01-08 22:38:27 -0500

    Updates

---
+++ precache-manifest.547824957b027c5e36b6a0212badf9b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a439e9b596542893623604a9e3f4ae99",
+    "url": "/restaurant-journey-app-frontend/index.html"
+  },
+  {
+    "revision": "5400d12f65e0f9fd662b",
+    "url": "/restaurant-journey-app-frontend/static/css/main.68abee3f.chunk.css"
+  },
+  {
+    "revision": "366b61bcde9632af86fa",
+    "url": "/restaurant-journey-app-frontend/static/js/2.1bc33514.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/restaurant-journey-app-frontend/static/js/2.1bc33514.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5400d12f65e0f9fd662b",
+    "url": "/restaurant-journey-app-frontend/static/js/main.30918c5e.chunk.js"
+  },
+  {
+    "revision": "577fde2e91e3c819153a",
+    "url": "/restaurant-journey-app-frontend/static/js/runtime-main.b1c3ba37.js"
+  },
+  {
+    "revision": "fcf27836aa2c3eb95a9f7037d49232b6",
+    "url": "/restaurant-journey-app-frontend/static/media/brooklyn.fcf27836.jpg"
+  },
+  {
+    "revision": "b286e9f7e209b80a4a96fb7cc3555fef",
+    "url": "/restaurant-journey-app-frontend/static/media/fordham-road.b286e9f7.jpg"
+  },
+  {
+    "revision": "eb976ba66af5de29cf175e6ba321d3ea",
+    "url": "/restaurant-journey-app-frontend/static/media/manhattan.eb976ba6.jpg"
+  },
+  {
+    "revision": "441423d1c2b0321acacb9e3b110be6af",
+    "url": "/restaurant-journey-app-frontend/static/media/queens.441423d1.jpg"
+  },
+  {
+    "revision": "067766d4e022192902271eae2f691ed1",
+    "url": "/restaurant-journey-app-frontend/static/media/statenisland.067766d4.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/restaurant-journey-app/precache-manifest.26a11500a8e39ea80126c93882eceb1c.js"
+  "/restaurant-journey-app-frontend/precache-manifest.547824957b027c5e36b6a0212badf9b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/restaurant-journey-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/restaurant-journey-app-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
